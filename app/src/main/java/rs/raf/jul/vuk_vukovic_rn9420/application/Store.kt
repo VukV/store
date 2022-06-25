a81c3bb7cd599c2c33d2369ad540614b387147a4
VukV <vuk.vukovic.2000@gmail.com>
2022-06-25 13:58:45 +0200

    project reorganisation

---
+++ app/src/main/java/rs/raf/jul/vuk_vukovic_rn9420/application/Store.kt
@@ -0,0 +1,12 @@
+package rs.raf.jul.vuk_vukovic_rn9420.application
+
+import android.app.Application
+
+class Store : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        //todo
+    }
+}
