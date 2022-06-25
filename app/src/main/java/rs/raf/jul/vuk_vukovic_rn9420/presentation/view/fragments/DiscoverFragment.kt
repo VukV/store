e251ebefc2de09b8bfac90d723cb24139872e438
VukV <vuk.vukovic.2000@gmail.com>
2022-06-25 22:11:07 +0200

    working on products recycler view

--- app/src/main/java/rs/raf/jul/vuk_vukovic_rn9420/presentation/view/fragments/DiscoverFragment.kt
+++ app/src/main/java/rs/raf/jul/vuk_vukovic_rn9420/presentation/view/fragments/DiscoverFragment.kt
@@ -5,9 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import rs.raf.jul.vuk_vukovic_rn9420.R
 import rs.raf.jul.vuk_vukovic_rn9420.databinding.FragmentDiscoverBinding
-import rs.raf.jul.vuk_vukovic_rn9420.databinding.FragmentProfileBinding
 
 class DiscoverFragment : Fragment(R.layout.fragment_discover) {
 
@@ -21,5 +21,26 @@ class DiscoverFragment : Fragment(R.layout.fragment_discover) {
     ): View? {
         _binding = FragmentDiscoverBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initSpinner()
+        initRecycler()
+        initListeners()
+    }
+
+    private fun initSpinner(){
+
+    }
+
+    private fun initRecycler(){
+        binding.productsRecycler.layoutManager = LinearLayoutManager(context)
+
+    }
+
+    private fun initListeners(){
+
     }
 }
